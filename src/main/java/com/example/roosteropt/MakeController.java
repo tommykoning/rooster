6c5d7bad9d54977c6b1c77e793641ea0337febd1
tommy <tommykoning@live.nl>
2022-03-31 00:56:53 +0200

    Update MakeController.java and Make-view.fxml: button to indicate if the rule is voor avalibility of unavalibility is dynamic

--- src/main/java/com/example/roosteropt/MakeController.java
+++ src/main/java/com/example/roosteropt/MakeController.java
@@ -1,13 +1,19 @@
 package com.example.roosteropt;
 
 import com.example.roosteropt.ScreenController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-
+import javafx.scene.control.ToggleButton;
 
 public class MakeController extends ScreenController {
 
     @FXML
-    private ComboBox<String> aval;
+    protected void aval(ActionEvent event) {
+        if (((ToggleButton)event.getSource()).getText().equals("avalible")) {
+            ((ToggleButton)event.getSource()).setText("unavalible");
+        } else {
+            ((ToggleButton)event.getSource()).setText("avalible");
+        }
+    }
 
 }
