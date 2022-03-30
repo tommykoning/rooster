880c4fef7fb3a33ee06cdabba4fb1c92b7c737f2
tommy <tommykoning@live.nl>
2022-03-31 00:57:15 +0200

    Update MenuController.java: unused code deleted

--- src/main/java/com/example/roosteropt/MenuController.java
+++ src/main/java/com/example/roosteropt/MenuController.java
@@ -10,16 +10,5 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MenuController extends ScreenController {
-
-
-    @FXML
-    private Label welcomeText;
-    private Button make;
-    private Button view;
-
-    @FXML
-    public static FXMLLoader scene() {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Menu-view.fxml"));
-        return fxmlLoader;
-    }
+    //
 }
