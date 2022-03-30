068b1462f5c8c922cf951ccd216038b9450ee6dc
tommy <tommykoning@live.nl>
2022-03-30 12:50:46 +0200

    Update rooster-opt.iml, Main.java, and 4 more files...: made small changes to the innitial setup to fit the project more

--- src/main/java/com/example/roosteropt/MenuController.java
+++ src/main/java/com/example/roosteropt/MenuController.java
@@ -3,12 +3,17 @@ package com.example.roosteropt;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MenuController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void make() {
+        welcomeText.setText("dit hoort naar make te lijden");
+    }
+
+    @FXML
+    protected void view() {
+        welcomeText.setText("dit hoort naar view te lijden");
     }
 }
